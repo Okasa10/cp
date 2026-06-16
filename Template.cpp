8942df9eb0ac158944f2e86fa4260d6f7086b7e1
Okasa <kaamkijiye@gmail.com>
2026-06-16 17:30:58 +0530

    DayNo-Level- Count-Time

---
+++ Template.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+#define ll long long
+#define forn(start,n) for (int i = (start); i < (n); i++)
+
+
+
+void solve(){
+  
+}
+
+int main(){
+
+  int t ;
+  cin >> t;
+  while (t--)
+  {
+     solve();
+  }
+  
+  return 0;
+}
